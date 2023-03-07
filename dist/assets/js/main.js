12475106a65d6ebe17f748fa7dac872914b80103
shadowgks <saad.moumou.coder@gmail.com>
2023-03-07 23:11:46 +0100

    responsive navbar done

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,9 +1,15 @@
 function Menu(e){
+    const link = document.querySelector('#links-nav');
     if(e.getAttribute("class") == 'fa-solid fa-bars'){
         e.classList.remove("fa-bars");
         e.classList.add("fa-xmark");
+        console.log(link);
+        //link show
+        link.classList.add('top-[80px]');
     }else{
         e.classList.add("fa-bars");
         e.classList.remove("fa-xmark");
+        //link show
+        link.classList.remove('top-[80px]');
     }
 } 
