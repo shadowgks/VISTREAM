ac659cb2199906254e2bce0c3e0e4805fbbae8ba
shadowgks <saad.moumou.coder@gmail.com>
2023-03-08 23:16:19 +0100

    card 80% is completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+const Myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
@@ -9,7 +27,10 @@ module.exports = {
         'color-three': '#bd5ad4',
         'color-links': '#E9AAF8'
       },
+      width: {
+        'container': '95%'
+      }
     },
   },
-  plugins: [],
+  plugins: [Myclass],
 }
