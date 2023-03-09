d2fb9c30b5c690d8b5672ce577305d15bb2520c9
shadowgks <saad.moumou.coder@gmail.com>
2023-03-09 17:11:38 +0100

    fix tabs movies and series

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin");
-
-const Myclass = plugin(function ({ addUtilities }) {
-  addUtilities({
-    ".my-rotate-y-180": {
-      transform: "rotateY(180deg)",
-    },
-    ".preserve-3d": {
-      transformStyle: "preserve-3d",
-    },
-    ".perspective": {
-      perspective: "1000px",
-    },
-    ".backface-hidden": {
-      backfaceVisibility: "hidden",
-    },
-  });
-});
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: [
+    "./dist/*.{html,js}",
+    // "./dist/pages/*.{html,js}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -32,5 +17,5 @@ module.exports = {
       }
     },
   },
-  plugins: [Myclass],
+  plugins: [],
 }
