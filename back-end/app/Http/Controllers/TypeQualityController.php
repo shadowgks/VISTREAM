10d1af515f948813f51b6933e25535ff59227e1f
shadowgks <saad.moumou.coder@gmail.com>
2023-03-23 14:22:36 +0000

    create models & controller & migration tables

---
+++ back-end/app/Http/Controllers/TypeQualityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypeQuality;
+use Illuminate\Http\Request;
+
+class TypeQualityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TypeQuality  $typeQuality
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TypeQuality $typeQuality)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TypeQuality  $typeQuality
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TypeQuality $typeQuality)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TypeQuality  $typeQuality
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TypeQuality $typeQuality)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TypeQuality  $typeQuality
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TypeQuality $typeQuality)
+    {
+        //
+    }
+}
